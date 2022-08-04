30e1cf17d39b77481b808a32b2f94d2e1314c59e
Sri Sudarshan <srisudarshanrg@gmail.com>
2022-08-04 21:08:41 +0530

    calculator

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let enterNumbers = document.getElementById("enter_calc_num")
+function clickNum(number) {
+    alr_text = document.getElementById("enter_calc_num").innerText
+    document.getElementById("enter_calc_num").textContent = alr_text + ' ' + number
+}
 
-function click_num(number) {
-    enterNumbers.innerText += number
+function clearCalc() {
+    document.getElementById("enter_calc_num").textContent = ''
 }
